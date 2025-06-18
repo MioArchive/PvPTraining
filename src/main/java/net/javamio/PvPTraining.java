c5cfcfc5581d1ad5f71cf89892d6a0bc6e0ca533
Mio <140413118+MioArchive@users.noreply.github.com>
2025-06-18 21:18:07 +0200

    Add plugin initialization and configuration
    Introduce Bukkit plugin structure with `onEnable` method and singleton instance for `PvPTraining`. Add `plugin.yml` to define metadata such as name, version, main class, and author.

--- src/main/java/net/javamio/PvPTraining.java
+++ src/main/java/net/javamio/PvPTraining.java
@@ -1,4 +1,16 @@
 package net.javamio;
 
-public class PvPTraining {
+import lombok.Getter;
+import org.bukkit.plugin.java.JavaPlugin;
+
+@Getter
+public class PvPTraining extends JavaPlugin {
+
+    @Getter
+    public static PvPTraining instance;
+
+    @Override
+    public void onEnable() {
+        instance = this;
+    }
 }
